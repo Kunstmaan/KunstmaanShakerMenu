00a00d95498e3c745bc54e84ff0d560645b4a0cd
Michal Borkowski <michal.borkowski@kunstmaan.be>
2018-03-15 15:23:53 +0100

    bintray

--- app/src/main/java/be/kunstmaan/shakermenu/demo/MainActivity.java
+++ app/src/main/java/be/kunstmaan/shakermenu/demo/MainActivity.java
@@ -12,8 +12,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
-
     }
 }
